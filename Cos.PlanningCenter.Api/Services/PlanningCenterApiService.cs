b4cddfbed4d47153e38e366037723a6a52238350
Tommy Crews <tommy@crews.dev>
2024-05-13 11:45:41 -0500

    Refactor serialization and error handling.

--- Cos.PlanningCenter.Api/Services/PlanningCenterApiService.cs
+++ Cos.PlanningCenter.Api/Services/PlanningCenterApiService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using Cos.PlanningCenter.Api.Models;
 
 namespace Cos.PlanningCenter.Api;
@@ -9,14 +10,18 @@ namespace Cos.PlanningCenter.Api;
 public class PlanningCenterApiService(HttpClient httpClient)
 {
 	private readonly HttpClient _httpClient = httpClient;
+	private readonly JsonSerializerOptions _serializerOptions = new()
+	{
+		NumberHandling = JsonNumberHandling.AllowReadingFromString
+	};
 
-	/// <summary>
-	/// Fetches a collection of Planning Center data objects of the specific type.
-	/// </summary>
-	/// <param name="path">The URI of the API request.</param>
-	/// <typeparam name="T">The type of data object.</typeparam>
-	public Task<PlanningCenterRootCollectionObject<T>> GetAllAsync<T>(Uri path) where T : class 
-		=> SendRequestAsync<PlanningCenterRootCollectionObject<T>>(new()
+    /// <summary>
+    /// Fetches a collection of Planning Center data objects of the specific type.
+    /// </summary>
+    /// <param name="path">The URI of the API request.</param>
+    /// <typeparam name="T">The type of data object.</typeparam>
+    public Task<PlanningCenterRootCollectionObject<T>> GetAllAsync<T>(Uri path) where T : class 
+		=> SendRequestWithResponseAsync<PlanningCenterRootCollectionObject<T>>(new()
 		{
 			RequestUri = path,
 			Method = HttpMethod.Get
@@ -28,7 +33,7 @@ public class PlanningCenterApiService(HttpClient httpClient)
 	/// <param name="path">The URI of the API request.</param>
 	/// <typeparam name="T">The type of data object.</typeparam>
 	public Task<PlanningCenterRootSingletonObject<T>> GetAsync<T>(Uri path) where T : class 
-		=> SendRequestAsync<PlanningCenterRootSingletonObject<T>>(new()
+		=> SendRequestWithResponseAsync<PlanningCenterRootSingletonObject<T>>(new()
 		{
 			RequestUri = path,
 			Method = HttpMethod.Get
@@ -41,7 +46,7 @@ public class PlanningCenterApiService(HttpClient httpClient)
 	/// <param name="content">The collection of data objects to post.</param>
 	/// <typeparam name="T">The type of data object.</typeparam>
 	public Task<PlanningCenterRootCollectionObject<T>> PostAllAsync<T>(Uri path, IEnumerable<T> content) where T : class 
-		=> SendRequestAsync<PlanningCenterRootCollectionObject<T>>(new()
+		=> SendRequestWithResponseAsync<PlanningCenterRootCollectionObject<T>>(new()
 		{
 			RequestUri = path,
 			Method = HttpMethod.Post,
@@ -61,7 +66,7 @@ public class PlanningCenterApiService(HttpClient httpClient)
 	/// <param name="content">The data object to post.</param>
 	/// <typeparam name="T">The type of data object.</typeparam>
 	public Task<PlanningCenterRootSingletonObject<T>> PostAsync<T>(Uri path, T content) where T : class 
-		=> SendRequestAsync<PlanningCenterRootSingletonObject<T>>(new()
+		=> SendRequestWithResponseAsync<PlanningCenterRootSingletonObject<T>>(new()
 		{
 			RequestUri = path,
 			Method = HttpMethod.Post,
@@ -81,7 +86,7 @@ public class PlanningCenterApiService(HttpClient httpClient)
 	/// <param name="content">The data object with its modifications.</param>
 	/// <typeparam name="T">The type of data object.</typeparam>
 	public Task<PlanningCenterRootSingletonObject<T>> PatchAsync<T>(Uri path, T content) where T : class 
-		=> SendRequestAsync<PlanningCenterRootSingletonObject<T>>(new()
+		=> SendRequestWithResponseAsync<PlanningCenterRootSingletonObject<T>>(new()
 		{
 			RequestUri = path,
 			Method = HttpMethod.Patch,
@@ -98,25 +103,40 @@ public class PlanningCenterApiService(HttpClient httpClient)
 	/// Deletes one Planning Center data object.
 	/// </summary>
 	/// <param name="path">The URI of the API request.</param>
-	public Task DeleteAsync(Uri path) => _httpClient.SendAsync(new()
+	public Task DeleteAsync(Uri path) => SendRequestAsync(new()
 	{
 		RequestUri = path,
 		Method = HttpMethod.Delete
 	});
 
-	private async Task<U> SendRequestAsync<U>(HttpRequestMessage request)
+	private async Task<U> SendRequestWithResponseAsync<U>(HttpRequestMessage request)
+	{
+		HttpResponseMessage response = await SendRequestAsync(request);
+		return JsonSerializer.Deserialize<U>(await response.Content.ReadAsStringAsync(), _serializerOptions)
+			?? throw new NullReferenceException("The deserialized response object was null.");
+	}
+
+	private async Task<HttpResponseMessage> SendRequestAsync(HttpRequestMessage request)
 	{
 		HttpResponseMessage responseMessage = await _httpClient.SendAsync(request);
-		string content = await responseMessage.Content.ReadAsStringAsync();
 		if (!responseMessage.IsSuccessStatusCode)
 		{
-			PlanningCenterErrorResponse errorResponse = JsonSerializer.Deserialize<PlanningCenterErrorResponse>(content)
-				?? throw new HttpRequestException($"The HTTP request failed with code {responseMessage.StatusCode}.");
+			string content = await responseMessage.Content.ReadAsStringAsync();
+			if (string.IsNullOrWhiteSpace(content))
+			{
+				throw new HttpRequestException("The HTTP request failed "
+					+ $"({(int)responseMessage.StatusCode} {responseMessage.StatusCode}).");
+			}
+			PlanningCenterErrorResponse errorResponse = JsonSerializer.Deserialize<PlanningCenterErrorResponse>(
+				await responseMessage.Content.ReadAsStringAsync(), _serializerOptions) 
+				?? throw new HttpRequestException("The HTTP request failed "
+					+ $"({(int)responseMessage.StatusCode} {responseMessage.StatusCode}). "
+					+ "A response body was present, but could not be deserialized.");
 			throw new HttpRequestException(errorResponse.Errors
-				.Select(e => $"{e.HttpStatusCode} {e.Title}: {e.Details} [{e.ErrorCode}]")
+				.Select(e => $"{e.HttpStatusCode} {e.Title}: {e.Details} {(e.Metadata?.Description != null 
+					? $"({e.Metadata.Description}) " : "")}{(e.ErrorCode != null ? $"[{e.ErrorCode}]" : "")}")
 				.Aggregate((a, b) => $"{a}\n\n{b}"));
 		}
-		return JsonSerializer.Deserialize<U>(content)
-			?? throw new NullReferenceException("The deserialized response object was null.");
+		return responseMessage;
 	}
 }

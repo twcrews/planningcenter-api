4c52dd85039f3273f2708376cf7cb455a6be30b7
Tommy Crews <twcrews96@gmail.com>
2025-12-31 21:56:11 -0600

    Restore documentation parser application class.

--- Crews.PlanningCenter.Api.DocParser/Application.cs
+++ Crews.PlanningCenter.Api.DocParser/Application.cs
@@ -1,38 +1,44 @@
-﻿using Crews.PlanningCenter.Api.DocParser.Models;
+﻿using Crews.PlanningCenter.Api.DocParser.Configuration;
 using Crews.PlanningCenter.Api.DocParser.Services;
 using Crews.PlanningCenter.Api.Models;
+using Crews.PlanningCenter.Api.Models.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace Crews.PlanningCenter.Api.DocParser;
 
-class Application(ILogger<Application> logger, IConfiguration configuration, IPlanningCenterClient client)
+class Application(ILogger<Application> logger, IConfiguration configuration, IDocumentationBuilder documentationBuilder)
 {
     public async Task RunAsync()
     {
-        List<string> printedTypes = [];
+        logger.LogInformation("Documentation parsing started at {DateTime}", DateTime.Now.ToString());
 
-        foreach (ProductDefinition product in ProductDefinition.All)
+        AppSettings settings = new();
+        configuration.GetSection(nameof(AppSettings)).Bind(settings);
+        JsonSerializerOptions serializerOptions = new()
         {
-            GraphDocument graphDoc = await client.GetGraphAsync(product);
+            WriteIndented = true
+        };
 
-            foreach (string? version in graphDoc.Data.Relationships.Versions.Data.Select(v => v.Id))
+        logger.LogInformation("Fetching API documentation from Planning Center...");
+        IEnumerable<Product> products = await documentationBuilder.BuildAllProductsAsync();
+
+        string outputDir = settings.OutputDirectory ?? "output";
+        logger.LogInformation("Writing documentation to output directory: {OutputDirectory}", outputDir);
+
+        foreach (Product product in products)
+        {
+            string productDir = Path.Combine(outputDir, product.Name.ToString().ToPascalCase());
+            DirectoryInfo directory = Directory.CreateDirectory(Path.Combine(outputDir, product.Name.ToString().ToPascalCase()));
+            foreach (Api.Models.Version version in product.Versions)
             {
-                GraphVersionDocument versionDoc = await client.GetGraphVersionAsync(product, version!);
+                string path = Path.Combine(directory.FullName, $"{version.Id}.json");
+                logger.LogDebug("Writing documentation for product {Product} version {Version} to {Path}...", product.Name, version.Id, path);
 
-                foreach (string? vertex in versionDoc.Data.Relationships.Vertices.Data.Select(v => v.Id))
-                {
-                    VertexDocument vertexDoc = await client.GetVertexAsync(product, version!, vertex!);
-
-                    foreach (string type in vertexDoc.Data.Relationships!.Attributes.Data.Select(a => a.Attributes.TypeAnnotation.Name))
-                    {
-                        if (!printedTypes.Contains(type))
-                        {
-                            printedTypes.Add(type);
-                            logger.LogInformation("Discovered type '{Type}' at {Product}.{Version}.{Vertex}", type, product, version, vertex);
-                        }
-                    }
-                }
+                using FileStream stream = File.Create(path);
+                await JsonSerializer.SerializeAsync(stream, version, serializerOptions);
+                await stream.DisposeAsync();
             }
         }
     }

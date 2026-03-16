150e2fa9ba2d3ae5d9fb833e257630017c5f5b70
Tommy Crews <twcrews96@gmail.com>
2026-03-16 11:27:57 -0500

    Fix models tests.

--- Crews.PlanningCenter.Api.Models.Tests/ProductDefinitionTests.cs
+++ Crews.PlanningCenter.Api.Models.Tests/ProductDefinitionTests.cs
@@ -3,57 +3,65 @@ namespace Crews.PlanningCenter.Api.Models.Tests;
 public class ProductDefinitionTests
 {
     [Fact]
-    public void All_Contains_Eight_Products()
-        => Assert.Equal(8, ProductDefinition.All.Count());
-
-    [Fact]
     public void All_Contains_Expected_Products()
         => Assert.Equal(
             [
+                ProductDefinition.Api,
                 ProductDefinition.Calendar,
                 ProductDefinition.CheckIns,
+                ProductDefinition.Current,
                 ProductDefinition.Giving,
                 ProductDefinition.Groups,
                 ProductDefinition.People,
                 ProductDefinition.Publishing,
                 ProductDefinition.Registrations,
-                ProductDefinition.Services
+                ProductDefinition.Services,
+                ProductDefinition.Webhooks
             ],
             ProductDefinition.All);
 
     [Theory]
+    [InlineData("api")]
     [InlineData("calendar")]
     [InlineData("check-ins")]
+    [InlineData("current")]
     [InlineData("giving")]
     [InlineData("groups")]
     [InlineData("people")]
     [InlineData("publishing")]
     [InlineData("registrations")]
     [InlineData("services")]
+    [InlineData("webhooks")]
     public void All_Contains_Product_With_Name(string name)
         => Assert.Contains(ProductDefinition.All, p => (string)p == name);
 
     [Theory]
+    [InlineData("api")]
     [InlineData("calendar")]
     [InlineData("check-ins")]
+    [InlineData("current")]
     [InlineData("giving")]
     [InlineData("groups")]
     [InlineData("people")]
     [InlineData("publishing")]
     [InlineData("registrations")]
     [InlineData("services")]
+    [InlineData("webhooks")]
     public void ToString_Returns_Name(string name)
         => Assert.Contains(ProductDefinition.All, p => p.ToString() == name);
 
     [Theory]
+    [InlineData("api")]
     [InlineData("calendar")]
     [InlineData("check-ins")]
+    [InlineData("current")]
     [InlineData("giving")]
     [InlineData("groups")]
     [InlineData("people")]
     [InlineData("publishing")]
     [InlineData("registrations")]
     [InlineData("services")]
+    [InlineData("webhooks")]
     public void ImplicitStringConversion_Returns_Name(string name)
         => Assert.Contains(ProductDefinition.All, p => (string)p == name);
 }
